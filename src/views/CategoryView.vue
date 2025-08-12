7ca84a2e5e8c49d9ff19bbdb08a6ab17e0bb3e6e
ZIBAGO <cownsgur01@gmail.com>
2025-08-12 12:27:40 +0900

    refactor(views/CategoryView): watch 임포트 및 loadDescriptors로 로직 분리해 카테고리 변경 대응

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import type { AlgoCategory, AlgoDescriptor } from '@/types/step'
 import { initAlgorithms, listDescriptorsByCategory } from '@/algorithms/registry'
@@ -27,11 +27,14 @@ const descriptors = ref<AlgoDescriptor[]>([])
 const router = useRouter()
 const announcer = useA11yAnnouncements()
 
-onMounted(async () => {
+async function loadDescriptors() {
   await initAlgorithms()
   descriptors.value = listDescriptorsByCategory(props.category)
   announcer.announce(`${descriptors.value.length}개의 알고리즘이 있습니다.`)
-})
+}
+
+onMounted(loadDescriptors)
+watch(() => props.category, loadDescriptors)
 
 function open(d: AlgoDescriptor) {
   announcer.announce(`${d.title} 알고리즘 페이지로 이동합니다.`)
